662c3aa835855fb82f915571bf665b371a9118e8
Hilligans <37002538+Hilligans@users.noreply.github.com>
2021-05-07 00:44:56 -0400

    Add files via upload

---
+++ src/OurcraftLauncher.py
@@ -0,0 +1,14 @@
+import platform
+from WindowsInstaller import WindowsInstaller
+
+
+def main() -> None:
+    operating_system = platform.system()
+    if operating_system == "Windows":
+        WindowsInstaller().install()
+    else:
+        print("Your Operating system is not currently supported! Check back soon.")
+    return 
+
+if __name__ == '__main__':
+    main()
---
+++ src/SETTINGS.py
@@ -0,0 +1,19 @@
+JDK_DIR = "jdk-16"
+WINDOWS_JDK_DOWNLOAD_URL = "https://download.java.net/java/GA/jdk16/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-16_windows-x64_bin.zip"
+JDK_DOWNLOAD_FILENAME = "jdk16.zip"
+
+MAVEN_DIR = "apache-maven-3.8.1"
+WINDOWS_MAVEN_DOWNLOAD_URL = "https://mirror.its.dal.ca/apache/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.zip"
+MAVEN_DOWNLOAD_FILENAME = "maven3.8.1.zip"
+
+
+GAME_VERSIONS_DIR = "versions"
+GAME_BRANCH = "main"
+GAME_DOWNLOAD_URL = f"https://github.com/Hilligans/Ourcraft/archive/refs/heads/{GAME_BRANCH}.zip"
+GANE_DOWNLOAD_FILENAME = "ourcraft-latest.zip"
+GAME_GITHUB_LINK = f"https://github.com/Hilligans/Ourcraft/commits/{GAME_BRANCH}"
+
+
+
+PROJECT_NAME = "Ourcraft"
+TEMP = "tmp"
